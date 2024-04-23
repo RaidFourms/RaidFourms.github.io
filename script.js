66fb52ad7f303ce38130d8aed6a4ee97030c76ef
Omnipotent <44111484+RaidFourms@users.noreply.github.com>
2024-04-23 15:02:41 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const apiRoute = "https://rooik.at/v3/";
+const frontendRoute = "http://localhost:3000/";
+
+function request(path) {
+    return fetch(route + path)
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
+}
+
+function search(nick) {
+    return request("player/search/?nick=" + nick);
+}
+
+function searchPlayer() {
+    const searchInput = document.getElementById("searchInput").value;
+    window.location.href = `${frontendRoute}p/?nick=${searchInput}`
+    
+}
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+}
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("collapsed");
+}
+
+// request('player/search/?nick=redllama')
